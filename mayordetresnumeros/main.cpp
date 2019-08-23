0e10f4355c58f645aa9f6a82e1a04949d3df2ea5
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-08-23 00:39:43 -0500

    Update main.cpp

--- mayordetresnumeros/main.cpp
+++ mayordetresnumeros/main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    int b;
+    int c;
+    cout << "ingresa un numero: ";
+    cin >> a;
+
+    cout << "ingresa un numero: ";
+    cin >> b;
+
+    cout << "ingresa un numero: ";
+    cin >> c;
+
+    if(a>b && a>c)
+        cout << "el mayor es " << a <<endl;
+    else if (b>a && b>c)
+        cout << "el mayor es " << b <<endl;
+    else
+        cout << "el mayor es " << c <<endl;
+
+
     return 0;
 }
