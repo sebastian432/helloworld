1afdde9c73b66dbae6157107a6a2a2ac322af47c
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-09-26 23:24:58 -0500

    Update arreglos.cpp

--- sebastarea/arreglos.cpp
+++ sebastarea/arreglos.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
 using namespace std;
-/*
+
 void imprimir(int arr[],int tam){
         for (int i=0;i<tam;i++){
             cout<<arr[i]<<endl;
         }
 }
-
 int main(){
         int x[5];
         for(int i=0;i<5;i++){
@@ -18,6 +17,8 @@ int main(){
 }
 
 
+
+//suma de elementos
 int suma(int arr[],int tam){
     int suma=0;
     for(int i=0;i<tam;i++){
@@ -26,7 +27,6 @@ int suma(int arr[],int tam){
     cout<<"la suma de su arreglo es: "<<suma<<endl;
     return suma;
 }
-
 int main(){
     int x[5];
     for (int i=0;i<5;i++){
@@ -34,42 +34,37 @@ int main(){
         cin>>x[i];
     }cout<<"la suma de su arreglo es: "<<suma<<endl;
     suma(x,5);
-
 }
 
-
-//no sale recursiva
 int suma (int arr[],int tam){
-
     if(tam==1){
         return arr[0];
     }
     else
         return arr[tam-1]+suma(arr,tam-1);
-
 }
-
 int main(){
     int tam;
     cout<<"cuantos valores deseas?: ";
     cin>>tam;
-
     int x[tam];
     for (int i=0;i<tam;i++){
         cout<<"ingrese los numeros del arreglo: ";
         cin>>x[i];
     }cout<<"la suma de su arreglo es: "<<suma(x,tam)<<endl;
-
-
 }
-*/
+
 
 //invertir un arreglo
 
-void swap(int &arr[], int &tam){
-    int temp=x;
-    x=y;
-    y=temp;
+void swap(int &x, int &y){
+    int v=y/2;
+    y=1;
+    for (int i=0;i<v;i++){
+        int c=x[i];
+        x[i]=x[y];
+        x[y--]=c;
+    }
 }
 
 int main(){
@@ -78,10 +73,119 @@ int main(){
     for (int i=0;i<5;i++){
         cout<<"ingrese los numeros del arreglo: ";
         cin>>x[i];
-    }cout<<"el arreglo invertido es: "<<endl;
-    swap(x,5);
+    }
+    swap(x,5)
 
 }
 
 
+//ordenamiento burbuja
 
+
+
+int burbuja(int arr[],int tam){
+    int temp;
+        for (int i=0;i<tam;i++){
+            for(int j=0;j<tam;j++){
+                if (arr[j] > arr[j+1]){
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+            }
+            for(i=0;i<tam;i++){
+            cout<<arr[i]<<endl;
+        }
+        }
+
+}
+
+int main(){
+    int tam;
+    cout<<"cuantos valores deseas?: ";
+    cin>>tam;
+    int x[tam];
+    for (int i=0;i<tam;i++){
+        cout<<"ingrese los numeros del arreglo: ";
+        cin>>x[i];
+    }cout<<burbuja(x,tam)<<endl;
+}
+
+
+//insertionsort
+
+int insertionsort(int arr[],int tam){
+            int pos,temp;
+
+            for(int i=0;i<tam;i++){
+                pos=i;
+                temp=arr[i];
+                while ((pos>0) && (arr[pos-1]>temp)){
+                    arr[pos]=arr[pos-1];
+                    pos--;
+                }
+                arr[pos]=temp;
+        }
+        for(int i=0;i<tam;i++){
+            cout<<arr[i]<<endl;
+
+
+            }
+
+}
+
+int main(){
+int tam;
+    cout<<"cuantos valores deseas?: ";
+    cin>>tam;
+    int x[tam];
+    for (int i=0;i<tam;i++){
+        cout<<"ingrese los numeros del arreglo: ";
+        cin>>x[i];
+    }cout<<insertionsort(x,tam)<<endl;
+}
+
+
+
+//quicksort
+
+int quicksort(int arr[],int inicio,int fin){
+        int medio,i,j,piv;
+        medio=(inicio + fin)/2;
+        piv=arr[medio];
+        i=inicio;
+        j=fin;
+        while(i<=j){
+            while(arr[i] < piv){
+                i++;
+
+            while(arr[i] < piv){
+                i++;
+            if(i<=j){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                j++;
+                j--;
+            }
+
+        }
+        }
+
+
+
+}
+
+int main(){
+    int tam;
+    cout<<"cuantos valores deseas?: ";
+    cin>>tam;
+    int x[tam];
+    for (int i=0;i<tam;i++){
+        cout<<"ingrese los numeros del arreglo: ";
+        cin>>x[i];
+    }cout<<quicksort(x,0,tam-1)<<endl;
+
+
+}
