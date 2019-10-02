db166bde4de1f47e1b54174a49527aeff92689f8
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-10-01 22:59:24 -0500

    Update arreglos.cpp

--- sebastarea/arreglos.cpp
+++ sebastarea/arreglos.cpp
@@ -58,38 +58,33 @@ int main(){
 //invertir un arreglo
 
 void imprimir(int arr[], int tam)
-            {
-                for(int i = 0; i < tam; i++)
-                    cout << arr[i] << " ";
-                    cout << endl;
-            }
+{
+    for(int i = 0; i < tam; i++)
+        cout << arr[i] << " ";
+cout << endl;
+}
 
-            int swap(int &x, int &y){
-                        int temp = x;
-                            x = y;
-                            y = temp;
-            }
+int swap(int &x, int &y){
+int temp = x;
+    x = y;
+    y = temp;
+}
 
-            int invertir(int arr[], int tam)
-            {
-                for(int i = 0; i < tam/2; i++){
-            swap(arr[i], arr[tam-1-i]);
-                }
-            }
+int invertir(int arr[], int tam)
+{
+    for(int i = 0; i < tam/2; i++){
+    swap(arr[i], arr[tam-1-i]);
+    }
+}
 
-            int main()
-            {
-                int arr[5] = {1, 2, 3, 4, 5};
+int main()
+{
+    int arr[4] = {1, 2, 3, 4};
 
-            int a = 5;
-            int b = 10;
-            swap(a,b);
-            cout << a << " " << b << endl;
-
-            imprimir(arr, 5);
-            invertir(arr, 5);
-            imprimir(arr, 5);
-            }
+imprimir(arr, 4);
+invertir(arr, 4);
+imprimir(arr, 4);
+}
 
 
 //ordenamiento burbuja
