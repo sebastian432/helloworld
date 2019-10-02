bdc14db81879e258f7aad2d94278fe19c5a5670c
sebastian432 <54363983+sebastian432@users.noreply.github.com>
2019-10-02 00:09:46 -0500

    Update arreglos.cpp

--- sebastarea/arreglos.cpp
+++ sebastarea/arreglos.cpp
@@ -94,7 +94,7 @@ imprimir(arr, 4);
 int burbuja(int arr[],int tam){
     int temp;
         for (int i=0;i<tam;i++){
-            for(int j=0;j<tam;j++){
+            for(int j=0;j<(tam-1);j++){
                 if (arr[j] > arr[j+1]){
                     temp=arr[j];
                     arr[j]=arr[j+1];
@@ -197,3 +197,59 @@ int main(){
 
 
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        #include <iostream>  
+#include <stdlib.h>  
+
+using namespace std;
+
+void quickSort(int *A, int izq, int der) {
+
+ int piv = A[izq];
+ int i = izq;
+ int j = der;
+ int aux;
+
+ while (i < j) {
+  while (A[i] <= piv && i < j) i++;
+  while (A[j] > piv) j--;
+  if (i < j) {
+   aux  = A[i];
+   A[i] = A[j];
+   A[j] = aux;
+  }
+ }
+
+ A[izq] = A[j];
+ A[j] = piv;
+ if (izq<j - 1)
+  quickSort(A, izq, j - 1);
+ if (j + 1 <der)
+  quickSort(A, j + 1, der);
+
+}
+
+int main() {
+ int A[10];
+ cout << " El array de valores es: " << endl;
+ for (int i = 0; i < 10; i++) {
+  A[i] = rand();
+  cout << A[i] << " ";
+ }
+ cout << endl << " Y el array de valores ordenados es: " << endl;
+ quickSort(A, 0, 9);
+
+ for (int i = 0; i < 10; i++) {
+  cout << A[i] << " ";
+ }
+
+ return 0;
+}
+
